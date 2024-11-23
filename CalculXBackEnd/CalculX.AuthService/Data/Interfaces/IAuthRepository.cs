1c3e32e9ae21f5da5d86e470ea19d6acd21c3b35
Chamath Viduranga | BISTEC Global <chamath@bistecglobal.com>
2024-11-23 21:42:39 +0530

    auth service basic implementation

--- CalculXBackEnd/CalculX.AuthService/Data/Interfaces/IAuthRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using CalculX.AuthService.Models;
-
-namespace CalculX.AuthService.Data.Interfaces
-{
-    public interface IAuthRepository
-    {
-        Task<Auth> GetAuthorization(Auth auth);
-    }
-}
--- CalculXBackEnd/CalculX.AuthService/Program.cs
+++ CalculXBackEnd/CalculX.AuthService/Program.cs
@@ -1,8 +1,9 @@
-using CalculX.AuthService.Data;
-using CalculX.AuthService.Data.Interfaces;
-using CalculX.AuthService.Services;
-using CalculX.AuthService.Services.Interfaces;
+using CalculX.AuthService.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,25 +14,51 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<IAuthRepository, AuthRepository>();
-builder.Services.AddScoped<IAuthorizationService, AuthorizationService>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
