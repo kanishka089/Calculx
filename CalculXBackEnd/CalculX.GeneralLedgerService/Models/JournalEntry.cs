c38b9e2328858712931f87f120d4fbe6417359ba
Kanishka Yohan <kanishka.gunawardana@gmail.com>
2024-11-24 07:07:07 +0530

    hangfire added

---
+++ CalculXBackEnd/CalculX.GeneralLedgerService/Models/JournalEntry.cs
@@ -0,0 +1,13 @@
+﻿namespace CalculX.GeneralLedgerService.Models
+{
+    public class JournalEntry
+    {
+        public int JournalEntryId { get; set; }
+        public int TransactionId { get; set; }
+        public Transaction Transaction { get; set; }
+        public int AccountId { get; set; }
+        public Account Account { get; set; }
+        public decimal Debit { get; set; }
+        public decimal Credit { get; set; }
+    }
+}
